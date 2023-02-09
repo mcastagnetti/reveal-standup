1514d0649b733164eb97c8e8fb9c397a527be661
Marc-Olivier Castagnetti <marcolivier.castagnetti@gmail.com>
2023-02-09 21:52:26 +0100

    Style app

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,15 @@
 
 <style lang="postcss">
 	:global(html) {
-		height: 100vh;
+		min-height: 100vh;
 		background: linear-gradient(to bottom, #be4bdb 5%, #9775fa 95%);
+	}
+
+	/* Avoid Chrome to see Safari hack */
+	@supports (-webkit-touch-callout: none) {
+		:global(html) {
+			/* The hack for Safari */
+			min-height: -webkit-fill-available;
+		}
 	}
 </style>
